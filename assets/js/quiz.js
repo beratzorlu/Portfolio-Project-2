44083a1d54c6fd949a60a22b52f89e6e95d1ad1d
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-12 00:56:44 +0000

    Change content to explain startGame()

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,38 +1,42 @@
-const QUESTION = document.querySelector('#question');
-const CHOICES = Array.from(document.querySelectorAll('.choice-text'));
-const PROGRESSTEXT = document.querySelector('#progress-text');
-const SCORETEXT = document.querySelector('#score');
-const PROGRESSBARFULL = document.querySelector('#progress-bar-full');
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 const SCORE_POINTS = 10;
 const MAX_QUESTIONS = 10;
 
-let activeQuestion = {};
-let answerAccept = true;
+let currentQuestion = {};
+let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
-let quizQuestions = [];
+let availableQuestions = [];
 
 /* Object containing the questions of the quiz game. */
 let questions = [
     {
         question: 'According to Roman mythology, who was the father of Romulus and Remus?',
-        option1: 'A wolf',
-        option2: 'King Numitor',
-        option3: 'Jupiter',
-        option4: 'Mars',
+        choice1: 'A wolf',
+        choice2: 'King Numitor',
+        choice3: 'Jupiter',
+        choice4: 'Mars',
         answer: 4,
     }
 ]
 
-/*  */
-function startQuiz() {
+/** 
+ * Start the game by setting the default value to the score counter 
+ * and calling the first question 
+ */
+
+function startGame() {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions]; /* Spread operator */
-    newQuestion()
+    getNewQuestion()
 }
 
-function newQuestion () {
+function getNewQuestion() {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('RecentScore', score);
 
