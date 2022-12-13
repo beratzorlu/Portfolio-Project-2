0ca0f7f5bc3f11b83b1904593b0e53b5adb8e68c
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-13 00:04:20 +0000

    Fix progress bar incrementation

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -45,7 +45,7 @@ function getNewQuestion() {
 
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
