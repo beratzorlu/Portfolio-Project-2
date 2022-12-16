6d3431e8332f4642d3576b5435b790dc24ac434f
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-16 21:39:43 +0000

    Add function to renable register button

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,4 +1,12 @@
+const HIGH_SCORE_LIMIT = 5;
 const userName = document.querySelector('#username');
 const scoreSaveButton = document.querySelector('#scoreSaveButton');
 const endScore = document.querySelector('#endScore');
 const recentScore = document.querySelector('#recentScore');
+const savedScores = JSON.parse(localStorage.getItem('savedScores')) || [];
+
+endScore.innerText = recentScore;
+
+userName.addEventListener('keyup', () => {
+    scoreSaveButton.disabled = !userName.value
+})
