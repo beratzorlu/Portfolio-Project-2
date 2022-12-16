8704488f5538ab176f7ac739818caefe2f4694bd
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-16 21:46:33 +0000

    Add event listener and saveHighScore()

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,4 +1,4 @@
-const HIGH_SCORE_LIMIT = 5;
+const HIGH_SCORE_LIMIT = 4;
 const userName = document.querySelector('#username');
 const scoreSaveButton = document.querySelector('#scoreSaveButton');
 const endScore = document.querySelector('#endScore');
@@ -9,4 +9,20 @@ endScore.innerText = recentScore;
 
 userName.addEventListener('keyup', () => {
     scoreSaveButton.disabled = !userName.value
-})
+});
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: recentScore,
+        name: userName.value,
+    }
+
+    savedScores.push(score);
+    savedScores.sort((a,b) => {
+        return b.score - a.score;
+    })
+
+    savedScores.splice(4);
+}
