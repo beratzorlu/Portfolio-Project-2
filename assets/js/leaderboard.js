c33de61b8e41d8d3b8a862bc1d5290e3c8abcce1
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-16 22:13:02 +0000

    Add function to display user scores

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,2 +1,6 @@
 const savedScoresList = document.querySelector('#savedScoresList');
-const savedScores = JSON.parse(localStorage.getItem('savedScores'));
+const savedScores = JSON.parse(localStorage.getItem('savedScores')) || [];
+
+savedScoresList.innerHTML = savedScores.map(score => {
+    return `<li class="high-score">${score.name} earned ${score.score} points`
+})
