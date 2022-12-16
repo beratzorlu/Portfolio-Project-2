33033b963a655d5f4d0f8fec3175939d01944e73
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-16 21:36:03 +0000

    Add JS consts and CSS to result page

---
+++ assets/js/result.js
@@ -0,0 +1,4 @@
+const userName = document.querySelector('#username');
+const scoreSaveButton = document.querySelector('#scoreSaveButton');
+const endScore = document.querySelector('#endScore');
+const recentScore = document.querySelector('#recentScore');
