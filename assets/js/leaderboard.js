98531b3ff80ed66c4d82c20d9a75234622c7dee9
berat.zorlu <berat.zorlu@hotmail.com>
2022-12-16 22:15:44 +0000

    Add join() to combine genrated strings

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -2,5 +2,5 @@ const savedScoresList = document.querySelector('#savedScoresList');
 const savedScores = JSON.parse(localStorage.getItem('savedScores')) || [];
 
 savedScoresList.innerHTML = savedScores.map(score => {
-    return `<li class="high-score">${score.name} earned ${score.score} points`
-})
+    return `<li class="high-score">${score.name} earned ${score.score} points</li>`
+}).join('');
